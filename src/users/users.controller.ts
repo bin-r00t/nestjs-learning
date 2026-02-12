37684cfe4a897816b0cf04579a925e7d56612665
liubin <bh0bama@sina.com>
2026-02-12 16:02:23 +0800

    feat: add user logic, connect repository(typeorm) -> service -> controller together

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private userService: UsersService) {}
+
   @Post('/create')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    const { email, password } = body;
+    return this.userService.create(email, password);
+  }
+
+  @Get('/all')
+  getAllUsers() {
+    return this.userService.getAllUsers();
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  // access the user's repository:
+  // InjectRepository() is a special decorator that gives us access to the repository
+  // of the entity we specify in the parameter. In this case, we want to access the
+  // repository of the User entity, so we pass User as a parameter to the InjectRepository() decorator.
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  getAllUsers() {
+    return this.repo.find();
+  }
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+}
