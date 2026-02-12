860ea963d4102583f5e430122bc9f83d3825d637
liubin <bh0bama@sina.com>
2026-02-12 15:50:17 +0800

    feat: add CreateUserDto and use it in controller

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 全局注册 pipe
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // 过滤掉 DTO 中没有的属性
+      // forbidNonWhitelisted: true, // 如果 DTO 中没有的属性，抛出异常
+      // transform: true, // 自动转换 payload 中的类型
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
